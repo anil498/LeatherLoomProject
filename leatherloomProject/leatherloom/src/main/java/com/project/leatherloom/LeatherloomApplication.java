0e562e84eacb3b15d74afb3d1f03a5cba84dd7ad
anil.kumar <anil.kumar@mcarbon.com>
2024-10-17 00:02:12 +0530

    add spring boot project

---
+++ leatherloomProject/leatherloom/src/main/java/com/project/leatherloom/LeatherloomApplication.java
@@ -0,0 +1,13 @@
+package com.project.leatherloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeatherloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeatherloomApplication.class, args);
+	}
+
+}
---
+++ leatherloomProject/leatherloom/src/test/java/com/project/leatherloom/LeatherloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.leatherloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeatherloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
